c5669a41b03ff4c69c6137a40e0cf39e351dc23d
David Lin <davll.xc@gmail.com>
2021-07-09 00:57:57 +0800

    Refactor playback code

--- play.py
+++ play.py
@@ -1,5 +1,3 @@
-from time import monotonic_ns as abstime_ns
-from time import sleep
 import cv2
 import numpy as np
 
@@ -11,32 +9,57 @@ VFD_ROWS = 3
 IMG_W = VFD_COLS * VFD_CHAR_W
 IMG_H = VFD_ROWS * VFD_CHAR_H
 
-# Init the GPIO
+TARGET_FRAME_TIME = 1.0 / 30.0
 
-# Init the VFD
-
-# Open the video
-cap = cv2.VideoCapture("bad_apple.mp4")
-
-# Play the video
-while cv2.waitKey(30) != 27:
-    # Read a frame
-    retval, frame = cap.read()
-    if not retval:
-        break
-    # Display the frame
-    cv2.imshow("Bad Apple - Original", frame)
-    # Process the frame
-    image = cv2.resize(frame, (IMG_W, IMG_H), interpolation = cv2.INTER_AREA)
+def process_frame(frame):
+    image = frame[:,:,0]
+    image = cv2.resize(image, (IMG_W, IMG_H), interpolation = cv2.INTER_AREA)
     retval, image = cv2.threshold(image, 128, 1, 0)
-    image_pv = cv2.resize(image * 255, (IMG_W * 16, IMG_H * 16), interpolation = cv2.INTER_NEAREST)
-    cv2.imshow("Bad Apple - Processed", image_pv)
-    # Send data to the VFD
+    return image
+
+def show_processed_image(title, image):
+    view = cv2.resize(image * 255, (IMG_W * 16, IMG_H * 16), interpolation = cv2.INTER_NEAREST)
+    cv2.imshow(title, view)
+
+def generate_blocks(image):
     for i in range(VFD_ROWS):
         ys = i * VFD_CHAR_H
         ye = ys + VFD_CHAR_H
         for j in range(VFD_COLS):
             xs = j * VFD_CHAR_W
             xe = xs + VFD_CHAR_W
-            block = image[ys:ye,xs:xe]
+            yield (i, j, image[ys:ye,xs:xe])
 
+def display_block(i, j, blk):
+    pass
+
+#====================================================================
+# Main
+#====================================================================
+
+def main():
+    # Open the video
+    cap = cv2.VideoCapture("bad_apple.mp4")
+    # Play the video
+    while cv2.waitKey(33) != 27:
+        # Read a frame
+        retval, frame = cap.read()
+        if not retval:
+            break
+        # Display the frame
+        cv2.imshow("Bad Apple - Original", frame)
+        # Process the frame
+        image = process_frame(frame)
+        show_processed_image("Bad Apple - Processed", image)
+        # Send data to the VFD
+        for i, j, blk in generate_blocks(image):
+            display_block(i, j, blk)
+
+def cleanup():
+    pass
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        cleanup()
